845a6871a3d8406cb9b890442da619de06333437
Sean Blessing <snblessing@gmail.com>
2024-08-23 14:27:37 -0400

    chap7 delete demo

---
+++ 06-chap7-crud.sql
@@ -0,0 +1,13 @@
+-- DELETE STATEMENT
+SELECT *
+  FROM SalesPerson;
+
+-- CRUD: Create, Read, Update, Delete
+--SELECT *
+DELETE
+  FROM SalesPerson
+ WHERE Id = 1;
+
+-- Delete 'em all!
+DELETE
+  FROM SalesPerson;
