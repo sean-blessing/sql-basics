8c113ec8b9715138ee88f860a15ba27a0369a4a0
Sean Blessing <snblessing@gmail.com>
2024-08-23 14:58:29 -0400

    updates to bmdb script

--- bmdb-create-sqlserver.sql
+++ bmdb-create-sqlserver.sql
@@ -1,59 +1,42 @@
 USE MASTER;
 GO
 
-DROP DATABASE IF EXISTS SALESDB;
+DROP DATABASE IF EXISTS bmdb;
 GO
 
 
-CREATE DATABASE SALESDB;
+CREATE DATABASE bmdb;
 GO
 
-USE SALESDB;
+USE bmdb;
 GO
 
-CREATE TABLE Sales_Denormalized (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	SalesPersonId int NOT NULL,
-	SalesPersonFirstName varchar(25) NOT NULL,
-	SalesPersonLastName varchar(25) NOT NULL,
-	Region varchar(20) NOT NULL,
-	SalesDate date NOT NULL,
-	SalesAmount decimal(10,2),
-	Customer varchar(50)
+CREATE TABLE Movie (
+	Id			int				PRIMARY KEY IDENTITY(1,1),
+	Title		varchar(255)	NOT NULL,
+	Year		int				NOT NULL,
+	Rating		varchar(5)		NOT NULL,
+	Director	varchar(255)	NOT NULL,
+	CONSTRAINT UQ_Movie_Title_Year UNIQUE (Title, Year)
 );
 
--- name should be unique
-CREATE TABLE Region (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	-- Name varchar(15) NOT NULL UNIQUE
-	Name varchar(15) NOT NULL,
-	CONSTRAINT UQ_Region_Name UNIQUE (Name)
+CREATE TABLE Actor (
+	Id			int				PRIMARY KEY IDENTITY(1,1),
+	FirstName	varchar(255)	NOT NULL,
+	LastName	varchar(255)	NOT NULL,
+	Gender		varchar(1)		NOT NULL,
+	Birthdate	date			NOT NULL,
+	CONSTRAINT UQ_Actor_fn_ln_bd UNIQUE (FirstName, LastName, Birthdate)
 );
 
--- name should be unique
-CREATE TABLE Customer (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	Name varchar(50) NOT NULL,
-	CONSTRAINT UQ_Customer_Name UNIQUE (Name)
+CREATE TABLE Credit (
+	Id			int				PRIMARY KEY IDENTITY(1,1),
+	MovieId		int				NOT NULL,
+	ActorId		int				NOT NULL,
+	Role		varchar(255)	NOT NULL,
+	FOREIGN KEY (MovieId) REFERENCES Movie(Id),
+	FOREIGN KEY (ActorId) REFERENCES Actor(Id),
+	CONSTRAINT UQ_Credit_Movie_Actor UNIQUE (MovieId, ActorId)
 );
 
-
--- RegionId is a FK to Region(Id)
-CREATE TABLE SalesPerson (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	FirstName varchar(50) NOT NULL,
-	LastName varchar(50) NOT NULL,
-	RegionId int NOT NULL,
-	FOREIGN KEY (RegionId) REFERENCES Region(Id)
-);
-
-CREATE TABLE Sales (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	SalesPersonId int NOT NULL,
-	SalesDate	date NOT NULL,
-	SalesAmount decimal(10,2) NOT NULL,
-	CustomerId int NOT NULL,
-	FOREIGN KEY (SalesPersonId) REFERENCES SalesPerson(Id),
-	FOREIGN KEY (CustomerId) REFERENCES Customer(Id)
-);
 
